d1aff26ce158c7b8842abc362457a8453fa96103
Madeline Morris <madehopemorr@gmail.com>
2021-02-12 01:25:12 -0800

    added burgers_controller.js

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,9 @@
+//import express and burger.js
+var express = require("express");
+var burger = require("../models/burger.js");
+//create router for the app, export the router at end of file.
+var router = express.Router();
+
+//create get, post, put routes
+
+//module.exports = router;
