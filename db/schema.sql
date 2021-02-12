970da6b2d89c5e77a826aa2536f4878c1408e010
Madeline Morris <madehopemorr@gmail.com>
2021-02-12 01:25:52 -0800

    added schema and seeds to db

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE burgers_db;
+USE playlist_db;
+
+CREATE TABLE burgers(
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(155),
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Avacado Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Impossible Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double-Double Burger', FALSE);
