e8b1426f0464e920652e9cc24d7b496f1f8618e8
Madeline Morris <madehopemorr@gmail.com>
2021-02-12 01:27:12 -0800

    added server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var PORT = process.env.PORT || 8000;
+var app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var expressHandlebars = require("express-handlebars");
+
+app.engine("handlebars", expressHandlebars({ defaultLayout: "main "}));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Listening on port:%s", PORT);
+});
