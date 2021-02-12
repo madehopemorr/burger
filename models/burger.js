12b1177ed97bfc948e79bda24f47eb1e0cc1c0ec
Madeline Morris <madehopemorr@gmail.com>
2021-02-12 01:26:23 -0800

    required orm in models

---
+++ models/burger.js
@@ -0,0 +1,6 @@
+//import orm.js into burger.js
+var orm = require("../config/orm.js");
+//create code to call ORM functions using burger specific input for the ORM.
+
+//export at end of this file
+//module.exports = burger;
