5e03c382a43a614f1b5c45c11d2a2080eea4afd1
Madeline Morris <madehopemorr@gmail.com>
2021-02-18 14:08:32 -0800

    routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,5 +5,40 @@ var burger = require("../models/burger.js");
 var router = express.Router();
 
 //create get, post, put routes
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
 
-//module.exports = router;
+router.post("/api/burgers", function(req, res) {
+    burger.create([
+        "burger_name", "devoured"
+    ], [
+        req.body.name, req.body.devoured 
+    ], function(result) {
+        res.json({ id:result.insertId });
+    });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, function(result) {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+module.exports = router;
