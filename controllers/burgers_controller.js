2ca47fb2fa561843ef8c36dcc2ae8ec4bad1e63b
Madeline Morris <madehopemorr@gmail.com>
2021-03-03 00:50:16 -0800

    updated controller.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -16,23 +16,21 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res) {
-    burger.create([
-        "burger_name", "devoured"
-    ], [
-        req.body.name, req.body.devoured 
-    ], function(result) {
-        res.json({ id:result.insertId });
+    console.log(req.body)
+    burger.create(
+        req.body.burger_name
+    , function(result) {
+        res.redirect("/")
     });
 });
 
 router.put("/api/burgers/:id", function(req, res) {
     var condition = "id = " + req.params.id;
 
-    console.log("condition", condition);
+     console.log("condition", condition);
 
-    burger.update({
-        devoured: req.body.devoured
-    }, condition, function(result) {
+    burger.update(
+        condition, function(result) {
         if (result.changedRows == 0) {
             return res.status(404).end();
         } else {
